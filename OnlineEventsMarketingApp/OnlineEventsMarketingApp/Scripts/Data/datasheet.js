dd32236e41dfa811d9800bbf2b34481b0eeb2711
CAWI\User <cawicaancornelio@gmail.com>
2018-05-28 20:23:09 +0800

    Export Validation and some UI Changes

--- OnlineEventsMarketingApp/OnlineEventsMarketingApp/Scripts/Data/datasheet.js
+++ OnlineEventsMarketingApp/OnlineEventsMarketingApp/Scripts/Data/datasheet.js
@@ -1,14 +1,25 @@
 ﻿$(function () {
 
     function handleSubmit(e) {
+        if ($('#File').get(0).files.length === 0) {
+            alert('No file selected');
+            e.preventDefault();
+            return;
+        }
+
         var answer = confirm("Are you sure you want to submit this new datasheet? This will delete the existing one and replaced by this one.");
         if (!answer) {
             e.preventDefault();
         }
     }
 
+    function handleExportClick(e) {
+        
+    }
+
     function init() {
         $('form').on('submit', handleSubmit);
+        $('.js-export').on('click', handleExportClick);
     }
 
     init();
